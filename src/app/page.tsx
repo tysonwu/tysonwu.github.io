c5463ceffda858c7dcf7ca5ac52e3dc2bdfb99e1
Tyson Wu <wukwanyutyson@gmail.com>
2023-03-18 05:04:54 +0800

    init 👻

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <h1>tylab.dev</h1>
+      </div>
+    </main>
+  )
+}
