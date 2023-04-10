02e4eeb728a96786663faaf18130ef29b1be5bf7
Tyson Wu <wukwanyutyson@gmail.com>
2023-04-11 02:30:07 +0800

    tailwind and a random new look

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
-import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <main className={styles.main}>
-      <div className={styles.container}>
-        <div className={styles.title}>tylab.dev</div>
-        <p className={styles.findMe}><a href='https://github.com/tysonwu' target='blank'>Find me on GitHub</a></p>
+    <main className='p-20'>
+      <div className='bg-stone-100 w-2/3 m-auto font-mono text-center border border-b-4 border-r-4 border-stone-800 rounded-lg'>
+        <div className='my-10 p-20'>
+          <p className='text-stone-800 text-2xl py-1'>Hi, I am Tyson!</p>
+          <span className='my-1 hover:text-stone-600'><a href='https://github.com/tysonwu' target='blank'>Find me on GitHub</a></span>
+        </div>
       </div>
     </main>
   )
